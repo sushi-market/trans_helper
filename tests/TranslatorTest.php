5244d8b16b1433cb8f8de2b297d8b390ba8de88b
m0nclous <m0nclous@vk.com>
2023-08-03 18:00:39 +0600

    Вырезано дублирование _translation из названий тестов

--- tests/TranslatorTest.php
+++ tests/TranslatorTest.php
@@ -12,57 +12,57 @@ class TranslatorTest extends TestCase
         app()['path.lang'] = __DIR__ . '/lang';
     }
 
-    public function test_translation_simple(): void
+    public function test_simple(): void
     {
         $this->assertEquals('some simple value', ___('some_simple_key'));
     }
 
-    public function test_translation_simple_with_dot(): void
+    public function test_simple_with_dot(): void
     {
         $this->assertEquals('some simple value for key with dot', ___('some_simple.key_with_dot'));
     }
 
-    public function test_translation_simple_with_dot_and_variable1(): void
+    public function test_simple_with_dot_and_variable1(): void
     {
         $this->assertEquals('some simple Argh for key with dot', ___('some_simple.key_with_dot_variable1', ['variable' => 'Argh']));
     }
 
-    public function test_translation_simple_with_dot_and_variable2(): void
+    public function test_simple_with_dot_and_variable2(): void
     {
         $this->assertEquals('some simple Argh for key with dot', ___('some_simple.key_with_dot_variable2', ['variable' => 'Argh']));
     }
 
-    public function test_translation_simple_with_locale_ru(): void
+    public function test_simple_with_locale_ru(): void
     {
         $this->assertEquals('какое-то простое значение', ___('some_simple_key', locale: 'ru'));
     }
 
-    public function test_translation_simple_not_found(): void
+    public function test_simple_not_found(): void
     {
         $this->assertEquals('some_simple_key_wrong', ___('some_simple_key_wrong'));
     }
 
-    public function test_translation_complex(): void
+    public function test_complex(): void
     {
         $this->assertEquals('some complex value', ___('some.complex.key'));
     }
 
-    public function test_translation_complex_with_locale_ru(): void
+    public function test_complex_with_locale_ru(): void
     {
         $this->assertEquals('какое-то комплексное значение', ___('some.complex.key', locale: 'ru'));
     }
 
-    public function test_translation_complex_not_found(): void
+    public function test_complex_not_found(): void
     {
         $this->assertEquals('some.complex.wrong.key', ___('some.complex.wrong.key'));
     }
 
-    public function test_translation_complex_part_object_found(): void
+    public function test_complex_part_object_found(): void
     {
         $this->assertEquals('some.complex', ___('some.complex'));
     }
 
-    public function test_translation_variables_capital_first_letter(): void
+    public function test_variables_capital_first_letter(): void
     {
         $this->assertEquals('some Test value', ___('some.variable1.key_capital_first_letter', [
             'variable' => 'test'
@@ -73,7 +73,7 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_variables_capital_all_letters(): void
+    public function test_variables_capital_all_letters(): void
     {
         $this->assertEquals('some TEST value', ___('some.variable1.key_capital_all_letters', [
             'variable' => 'test'
@@ -84,7 +84,7 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_variables(): void
+    public function test_variables(): void
     {
         $this->assertEquals('some test value', ___('some.variable1.key', [
             'variable' => 'test'
@@ -95,7 +95,7 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_variables_twice(): void
+    public function test_variables_twice(): void
     {
         $this->assertEquals('some test value and test value', ___('some.variable1.key_twice', [
             'variable' => 'test'
@@ -106,7 +106,7 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_variables_many(): void
+    public function test_variables_many(): void
     {
         $this->assertEquals('some test value and other test value', ___('some.variable1.many_keys', [
             'variable' => 'test',
@@ -119,7 +119,7 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_variables_replace_error(): void
+    public function test_variables_replace_error(): void
     {
         $this->assertEquals('some :variable value', ___('some.variable1.key', [
             'variable_wrong' => 'test'
@@ -130,13 +130,13 @@ class TranslatorTest extends TestCase
         ]));
     }
 
-    public function test_translation_null_key(): void
+    public function test_null_key(): void
     {
         /** @noinspection PhpRedundantOptionalArgumentInspection */
         $this->assertEquals('', ___(null));
     }
 
-    public function test_translation_empty_key(): void
+    public function test_empty_key(): void
     {
         $this->assertEquals('', ___(''));
     }
