04cd23180bfb5cdb8a73e35ba762a3401fb29647
m0nclous <m0nclous@vk.com>
2023-08-03 14:35:35 +0600

    Рефакторинг, форматирование, новые тесты

---
+++ src/trans_helper.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (!function_exists('___')) {
+
+    /** Extract a key from localization in json object file delimiter "." */
+    function ___(string $key = null, array $replace = [], string $locale = null): string
+    {
+        if (is_null($key)) {
+            return '';
+        }
+
+        $key_list = explode('.', $key);
+
+        if (!is_array($key_list) || !count($key_list)) {
+            return trans($key, $replace, $locale);
+        }
+
+        $first_key = $key_list[0];
+        array_shift($key_list);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $translate = app('translator')->get($first_key, [], $locale);
+
+        if (empty($translate) || $translate == $first_key) {
+            return $key;
+        }
+
+        if (is_array($translate)) {
+            foreach ($key_list as $key_each) {
+                if (!isset($translate[$key_each])) {
+                    return $key;
+                }
+
+                if (is_string($translate[$key_each])) {
+                    $translate = &$translate[$key_each];
+                    break;
+                }
+
+                $translate = &$translate[$key_each];
+            }
+        }
+
+        if (!is_string($translate)) {
+            return $key;
+        }
+
+        $shouldReplace = [];
+        foreach ($replace as $key => $value) {
+            // Для локализаций переменных вида ":variable"
+            $shouldReplace[':' . Str::ucfirst($key)] = Str::ucfirst($value);
+            $shouldReplace[':' . Str::upper($key)] = Str::upper($value);
+            $shouldReplace[':' . $key] = $value;
+
+            // Для локализаций переменных вида "{variable}"
+            $shouldReplace['{' . Str::ucfirst($key) . '}'] = Str::ucfirst($value);
+            $shouldReplace['{' . Str::upper($key) . '}'] = Str::upper($value);
+            $shouldReplace['{' . $key . '}'] = $value;
+        }
+
+        return strtr($translate, $shouldReplace);
+    }
+}
---
+++ tests/TranslatorTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class TranslatorTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        app()['path.lang'] = __DIR__ . '/lang';
+    }
+
+    public function test_translation_simple(): void
+    {
+        $this->assertEquals('some simple value', ___('some_simple_key'));
+    }
+
+    public function test_translation_simple_with_dot(): void
+    {
+        $this->assertEquals('some simple value for key with dot', ___('some_simple.key_with_dot'));
+    }
+
+    public function test_translation_simple_with_locale_ru(): void
+    {
+        $this->assertEquals('какое-то простое значение', ___('some_simple_key', locale: 'ru'));
+    }
+
+    public function test_translation_simple_not_found(): void
+    {
+        $this->assertEquals('some_simple_key_wrong', ___('some_simple_key_wrong'));
+    }
+
+    public function test_translation_complex(): void
+    {
+        $this->assertEquals('some complex value', ___('some.complex.key'));
+    }
+
+    public function test_translation_complex_with_locale_ru(): void
+    {
+        $this->assertEquals('какое-то комплексное значение', ___('some.complex.key', locale: 'ru'));
+    }
+
+    public function test_translation_complex_not_found(): void
+    {
+        $this->assertEquals('some.complex.wrong.key', ___('some.complex.wrong.key'));
+    }
+
+    public function test_translation_complex_part_object_found(): void
+    {
+        $this->assertEquals('some.complex', ___('some.complex'));
+    }
+
+    public function test_translation_variables_capital_first_letter(): void
+    {
+        $this->assertEquals('some Test value', ___('some.variable1.key_capital_first_letter', [
+            'variable' => 'test'
+        ]));
+
+        $this->assertEquals('some Test value', ___('some.variable2.key_capital_first_letter', [
+            'variable' => 'test'
+        ]));
+    }
+
+    public function test_translation_variables_capital_all_letters(): void
+    {
+        $this->assertEquals('some TEST value', ___('some.variable1.key_capital_all_letters', [
+            'variable' => 'test'
+        ]));
+
+        $this->assertEquals('some TEST value', ___('some.variable2.key_capital_all_letters', [
+            'variable' => 'test'
+        ]));
+    }
+
+    public function test_translation_variables(): void
+    {
+        $this->assertEquals('some test value', ___('some.variable1.key', [
+            'variable' => 'test'
+        ]));
+
+        $this->assertEquals('some test value', ___('some.variable2.key', [
+            'variable' => 'test'
+        ]));
+    }
+
+    public function test_translation_variables_twice(): void
+    {
+        $this->assertEquals('some test value and test value', ___('some.variable1.key_twice', [
+            'variable' => 'test'
+        ]));
+
+        $this->assertEquals('some test value and test value', ___('some.variable2.key_twice', [
+            'variable' => 'test'
+        ]));
+    }
+
+    public function test_translation_variables_many(): void
+    {
+        $this->assertEquals('some test value and other test value', ___('some.variable1.many_keys', [
+            'variable' => 'test',
+            'other_variable' => 'other test',
+        ]));
+
+        $this->assertEquals('some test value and other test value', ___('some.variable2.many_keys', [
+            'variable' => 'test',
+            'other_variable' => 'other test',
+        ]));
+    }
+
+    public function test_translation_variables_replace_error(): void
+    {
+        $this->assertEquals('some :variable value', ___('some.variable1.key', [
+            'variable_wrong' => 'test'
+        ]));
+
+        $this->assertEquals('some {variable} value', ___('some.variable2.key', [
+            'variable_wrong' => 'test'
+        ]));
+    }
+
+    public function test_translation_null_key(): void
+    {
+        /** @noinspection PhpRedundantOptionalArgumentInspection */
+        $this->assertEquals('', ___(null));
+    }
+}
